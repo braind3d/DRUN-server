7b42e39b2ffc8f33f916b671fc8e0777712db5d0
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2022-05-18 19:39:01 +0300

    [Refactor] Web client remote access
    Regenerated Angular project to have remote access.
    References #1.

--- apps/drun-web-client/src/app/app.component.ts
+++ apps/drun-web-client/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'drun-server-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'drun-web-client';
